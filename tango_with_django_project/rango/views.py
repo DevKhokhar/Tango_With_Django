6db310934ed365aba2d9b655ac4877d2e7cdc8f6
Devangana <devangana5591@gmail.com>
2014-05-04 05:12:43 +0530

    Added forms for adding new category and new page

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from models import Category, Page
+from forms import CategoryForm, PageForm
 
 def index(request):
     context = RequestContext(request)
@@ -41,3 +42,35 @@ def decode_category_name_url(category_name_url):
 
 def encode_category_url(category_name):
     return category_name.replace(' ', '_')
+
+def add_category(request):
+    context = RequestContext(request)
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print form.errors
+    else:
+        form = CategoryForm()
+
+    return render_to_response('rango/add_category.html', {'form': form}, context)
+
+def add_page(request):
+    context = RequestContext(request)
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print form.errors
+    else:
+        form = PageForm()
+
+    return render_to_response('rango/add_page.html', {'form': form}, context)
